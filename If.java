08ab48ee05da7ed019436e17d1e7bc33b3c880e2
madhavi121098 <114555113+madhavi121098@users.noreply.github.com>
2022-09-30 11:15:39 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,22 @@
+class If
+{
+void add(int x, int y)
+{
+boolean resultXY=x>y;
+if(resultXY==true)
+{
+System.out.println("x is passitive");
+}
+else
+{
+System.out.println("x is negative");
+}
+}
+public static void main(String[]args)
+{
+If madhu=new If();
+madhu.add(3,4);
+}
+}
+
+
